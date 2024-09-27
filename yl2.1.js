79621c6a23fa360b9c08e067c774189cd7f76c38
Margit Viigi <margit.viigi@voco.ee>
2024-09-27 09:26:29 +0300

    Ylesanne 2.1

---
+++ yl2.1.js
@@ -0,0 +1,14 @@
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+rl.question('Sisestage temperatuur: ', temperatuur =>{
+    if (temperatuur < 4.0){
+      console.log('On jäätumise oht.')
+  } else {
+      console.log('Ei ole jäätumise ohtu.')
+  } 
+    rl.close()   
+})
